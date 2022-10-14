3a38a1827960e88b685b00ae39be751635254684
Yuheng Zhang <2845024327@qq.com>
2022-10-14 15:40:27 +0800

    try caiso oasis

--- pycaiso/get_lmp.py
+++ pycaiso/get_lmp.py
@@ -6,12 +6,12 @@ from pycaiso.oasis import Node
 
 # select pnode
 
-cj = Node("CAPTJACK_5_N003")
+cj = Node("NCMETER_1_N001")
 
 # create dataframe with LMPS from arbitrary period (30 day maximum). 
 
-cj_lmps = cj.get_lmps(datetime(2021, 1, 1), datetime(2021, 2, 1))
+cj_lmps = cj.get_lmps(datetime(2021, 1, 1), datetime(2021, 1, 2))
 
 print(cj_lmps.head())
 
-cj_lmps.to_csv("CAPTJACK_5_N003_2021.csv")
+cj_lmps.to_csv("NCMETER_1_N001.csv")
